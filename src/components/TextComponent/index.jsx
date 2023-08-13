f4b6134f96add776a134246af1b10f53723a1565
Weslley Addson <wesquel@hotmail.com>
2023-08-12 21:09:24 -0300

    created TextComponent

---
+++ src/components/TextComponent/index.jsx
@@ -0,0 +1,14 @@
+import P from "prop-types";
+import * as Styled from "./styles";
+
+export const TextComponent = ({ children }) => {
+  return (
+    <Styled.Container>
+      <p className="text">{children}</p>
+    </Styled.Container>
+  );
+};
+
+TextComponent.propTypes = {
+  children: P.node.isRequired,
+};
---
+++ src/components/TextComponent/styles.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.p`
+  ${({ theme }) => css`
+    ${theme}
+
+    font-weight: 200;
+    font-size: 1.8rem;
+    text-align: justify;
+  `}
+`;
